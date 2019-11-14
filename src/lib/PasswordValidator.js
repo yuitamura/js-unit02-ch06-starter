22a6b70908221ff1ae25b95f580b13222e8818aa
yuitamura <yuixtamura@gmail.com>
2019-11-14 23:42:05 +0100

    Updated function checkFormat

--- src/lib/PasswordValidator.js
+++ src/lib/PasswordValidator.js
@@ -8,6 +8,7 @@ export default class extends BaseValidator {
   validate() {
     return super._cannotEmpty()
       .then(this._checkLength)
+      .then(this._checkFormat)
       .then((res) => {
         return { success: true }; // Promise.resolve({ success: true })と同一
       })
@@ -24,6 +25,19 @@ export default class extends BaseValidator {
         type: 'password',
         message: 'パスワードが短すぎます。'
       });
+    }
+  }
+  _checkFormat() {
+    const re = /^[a-zA-Z0-9_.-@][A-Z+][_.-@+]*$/;
+    const match = re.test(this.val); // マッチするならtrue、しないならfalseを返す。
+    if (match) {
+      return Promise.resolve();
+    } else {
+      return Promise.reject({
+        success: false,
+        type: this.type,
+        message: `${this.typeName}には半角英数字または@_-.の4つの記号のみを利用可能です。1文字以上の大文字のアルファベットと記号(_-.@)を含む必要があります。`
+      })
     }
   }
 }
